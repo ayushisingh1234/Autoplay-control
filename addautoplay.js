9eedc9b583a3e9d7a28c73f3c7877897b6b92871
Ayushi <78724686+ayushisingh1234@users.noreply.github.com>
2021-09-20 11:33:06 +0530

    autoplay to work use the iframe player api

---
+++ addautoplay.js
@@ -0,0 +1,20 @@
+// Load the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// Replace the 'ytplayer' element with an <iframe> and
+// YouTube player after the API code downloads.
+var player;
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+        height: '480',
+        width: '853',
+        videoId: 'JW5meKfy3fY',
+        playerVars: {
+            'autoplay': 1,
+            'showinfo': 0,
+            'controls': 0
+        }
+    });
+}
